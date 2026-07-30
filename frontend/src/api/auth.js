5cfb01c7fb67b04c2a0ce057affc0f76e7c15e84
laibahanwer-28 <laibah.anwer@gmail.com>
2026-07-31 01:59:09 +0500

    Update frontend API URL for production

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://skinbestfr.onrender.com/api/auth",
 });
-
 export const registerUser = (userData) =>
   API.post("/register", userData);
 
